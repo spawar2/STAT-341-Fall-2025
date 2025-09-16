465c1ef489b80e1d7652f6771cfa42f3ea9f1a9b
Pawar <pawar1550@gmail.com>
2025-09-17 04:50:31 +0530

    Add files via upload

---
+++ Microarray-Analysis.R
@@ -0,0 +1,11 @@
+# Author: Pawar, Date: 09/16/2025, Purpose: Microarray analysis
+
+# Install package affy
+# Load package affy
+
+library(affy)
+
+# Reading sample microarray data
+setwd("/Users/pawar/Desktop/data")
+
+
