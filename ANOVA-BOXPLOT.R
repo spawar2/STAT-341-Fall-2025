1708395adefa698b6434c162b41b18d9f784c25d
Pawar <pawar1550@gmail.com>
2025-09-24 02:18:45 +0530

    Add files via upload

--- ANOVA-BOXPLOT.R
+++ ANOVA-BOXPLOT.R
@@ -1,8 +1,19 @@
+# Author: Pawar; Date: 09/23/2025; Purpose: Test ANOVA
+
 library(dplyr) 
+
+# Read dummy dataset
 
 PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv" 
 
+# Load the data poisons.csv
+
 df <- read.csv(PATH) %>% select(-X) %>% mutate(poison = factor(poison, ordered = TRUE)) 
+
+
+
+
+
 
 glimpse(df)
 
